203bf5808bc133763120d3063057d3f5bd9c0421
Rehman Server <rehman.khan.300a@gmail.com>
2023-03-13 14:32:30 +0000

    made some css changes on the server side

--- event/settings.py
+++ event/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*y_ub(hfsx+aqikct=@7rkk4$s4$k58kw0wmx4-&4afcl_3d(f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['EventManagementSystem.pythonanywhere.com']
 
 
 # Application definition
