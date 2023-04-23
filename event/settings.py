e2c2468d142b20a67f224054344617c5c546e225
Rehman Khan <rehman.khan.300a@gmail.com>
2023-04-23 14:23:43 +0530

    updated staticRoot path

--- event/settings.py
+++ event/settings.py
@@ -164,6 +164,6 @@ MEDIA_URL = '/media/'
 if DEBUG:
   STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 else:
-  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+  STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
