988690cfee38c7ecee2703f47d52726bc556159f
Rehman Khan <rehman.khan.300a@gmail.com>
2023-06-14 20:36:23 +0530

    Update settings.py

--- event/settings.py
+++ event/settings.py
@@ -155,8 +155,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'eventmanagement313@gmail.com'
-EMAIL_HOST_PASSWORD = 'phjkhetorolaayhd'
+EMAIL_HOST_USER = 'novag388@gmail.com'
+EMAIL_HOST_PASSWORD = 'dchojmssuigqlltm'
 
 
 STATIC_URL = '/static/'
