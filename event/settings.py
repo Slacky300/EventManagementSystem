54de5e29e5adccd6a925f86bb24d14b1df0ce92c
Rehman Khan <rehman.khan.300a@gmail.com>
2023-06-14 20:46:20 +0530

    Update settings.py

--- event/settings.py
+++ event/settings.py
@@ -161,8 +161,8 @@ EMAIL_HOST_PASSWORD = 'dchojmssuigqlltm'
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/tmp/'
-STATICFILES_DIRS = os.path.join(BASE_DIR,'staticfiles_build','static'),
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # if DEBUG:
 #   STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # else:
